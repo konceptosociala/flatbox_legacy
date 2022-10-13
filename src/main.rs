c5f3cbc82470fc186d04768231d5cbd782fbf7c6
andezitgq <sanerhp.mail@gmail.com>
2022-10-13 10:01:20 +0300

    ŝanĝoĉenoj

--- src/main.rs
+++ src/main.rs
@@ -143,14 +143,52 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let surface_present_modes 	= unsafe { surface_loader.get_physical_device_surface_present_modes(physical_device, surface) };
     let surface_formats 		= unsafe { surface_loader.get_physical_device_surface_formats(physical_device, surface) };
     
-    dbg!(&surface_capabilities);
-    dbg!(&surface_present_modes);
-    dbg!(&surface_formats);
+    let queuefamilies = [qfamindices.0];
+    let swapchain_create_info = vk::SwapchainCreateInfoKHR::builder()
+        .surface(surface)
+        .min_image_count(
+            3.max(surface_capabilities.min_image_count)
+                .min(surface_capabilities.max_image_count),
+        )
+        .image_format(surface_formats.first().unwrap().format)
+        .image_color_space(surface_formats.first().unwrap().color_space)
+        .image_extent(surface_capabilities.current_extent)
+        .image_array_layers(1)
+        .image_usage(vk::ImageUsageFlags::COLOR_ATTACHMENT)
+        .image_sharing_mode(vk::SharingMode::EXCLUSIVE)
+        .queue_family_indices(&queuefamilies)
+        .pre_transform(surface_capabilities.current_transform)
+        .composite_alpha(vk::CompositeAlphaFlagsKHR::OPAQUE)
+        .present_mode(vk::PresentModeKHR::FIFO);
+    let swapchain_loader = ash::extensions::khr::Swapchain::new(&instance, &logical_device);
+    let swapchain = unsafe { swapchain_loader.create_swapchain(&swapchain_create_info, None)? };
+	let swapchain_images = unsafe { swapchain_loader.get_swapchain_images(swapchain)? };
+	
+	let mut swapchain_imageviews = Vec::with_capacity(swapchain_images.len());
+    for image in &swapchain_images {
+		let subresource_range = vk::ImageSubresourceRange::builder()
+            .aspect_mask(vk::ImageAspectFlags::COLOR)
+            .base_mip_level(0)
+            .level_count(1)
+            .base_array_layer(0)
+            .layer_count(1);
+        let imageview_create_info = vk::ImageViewCreateInfo::builder()
+			.image(*image)
+			.view_type(vk::ImageViewType::TYPE_2D)
+			.format(vk::Format::B8G8R8A8_UNORM)
+			.subresource_range(*subresource_range);
+		let imageview = unsafe { logical_device.create_image_view(&imageview_create_info, None) }?;
+		swapchain_imageviews.push(imageview);
+	}
     
     
     
     //Detruado
     unsafe { 
+		for iv in &swapchain_imageviews {
+            logical_device.destroy_image_view(*iv, None);
+        }
+		swapchain_loader.destroy_swapchain(swapchain, None);
 		logical_device.destroy_device(None);
 		debug_utils.destroy_debug_utils_messenger(utils_messenger, None);
 		surface_loader.destroy_surface(surface, None);
